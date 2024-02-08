d0944e0707d063fb350fb8a8ae3096d8df4a333e
Vibhu <vibhuk10@gmail.com>
2024-02-08 10:48:00 -0800

    update all files

--- src/alternative_reduce.py
+++ src/alternative_reduce.py
@@ -2,9 +2,13 @@
 
 # Arguments
 import argparse
+
+def list_of_strings(arg):
+    return arg.split(',')
+
 parser = argparse.ArgumentParser()
 #parser.add_argument('--input_paths',nargs='+',required=True)
-parser.add_argument('--keys',nargs='+',required=True)
+parser.add_argument('--keys',nargs='+', type=list_of_strings)
 args = parser.parse_args()
 
 # imports
@@ -18,20 +22,42 @@ import matplotlib.dates
 from datetime import datetime
 import glob
 
+print(args.keys[0])
 # load each of the input paths
-total = {key: [] for key in args.keys}
+total = {key: [] for key in args.keys[0]}
 paths = glob.glob('outputs/geoTwitter*.lang')
 for path in paths:
     date = os.path.splitext(os.path.basename(path))[0][10:18]
     with open(path) as f:
         tmp = json.load(f)
         for k in tmp:
-            print(args.keys)
-            if k in args.keys:
+            if k in args.keys[0]:
                 counts = 0
                 for key in tmp[k]:
                     counts += tmp[k][key]
-                print(counts)
                 total[k].append((date, counts))
 print(total)
+
 # make the plot
+for hashtag, date_count_pairs in total.items():
+    dates, counts = zip(*[(datetime.strptime(date, '%y-%m-%d'), count) for date, count in date_count_pairs])
+
+    # Sorting data based on dates
+    sorted_indices = sorted(range(len(dates)), key=lambda k: dates[k])
+    dates = [dates[i] for i in sorted_indices]
+    counts = [counts[i] for i in sorted_indices]
+
+    # Plotting
+    plt.plot(dates, counts, label=hashtag)
+
+# Formatting the plot
+plt.xlabel('Date')
+plt.ylabel('Count')
+plt.title('Hashtag Count Over Time')
+plt.legend()
+name = [] 
+for a in args.keys[0]:
+    name.append(a[1:])
+final_name = '_'.join(name)
+plt.savefig(final_name + '.png')
+
